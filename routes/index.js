0081b761c1ba09be8d864eeb121d46798a7be5f9
Jackson Lucas <jackson7br@gmail.com>
2015-11-27 13:07:37 -0400

    API path done, Building SQL query

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+var sequelize = require("../database/database_connection");
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+
+  sequelize.query("SELECT * FROM pessoa", { type: sequelize.QueryTypes.SELECT})
+    .then(function(pessoas) {
+      console.log(pessoas);
+      res.render('index', { title: 'Express' });
+    })
+});
+
+module.exports = router;
