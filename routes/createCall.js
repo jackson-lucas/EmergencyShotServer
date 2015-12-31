7bcd1abfbd2311609f8cdde3580dc62cc4b880ce
Jackson Lucas <jackson7br@gmail.com>
2015-12-31 15:07:33 -0400

    googleId removed due to API changes

--- routes/createCall.js
+++ routes/createCall.js
@@ -4,7 +4,6 @@ module.exports = function createCall (request, response, next) {
 
   console.log("CREATE CALL");
 
-  var googleId = request.body.googleId;
   var sinistroId = request.body.sinistroId;
   var data = request.body.data;
   var horario = request.body.horario;
@@ -12,8 +11,8 @@ module.exports = function createCall (request, response, next) {
   var longitude = request.body.lon;
   var midia = request.body.midia; // BLOB
 
-  var query = `INSERT INTO chamada(GOOGLE_ID, DATA, HORARIO, LAT, LON, ID_SINISTRO) VALUES
-    ('${googleId}', '${data}', '${horario}', ${latitude}, ${longitude}, ${sinistroId});`;
+  var query = `INSERT INTO chamada(DATA, HORARIO, LAT, LON, ID_SINISTRO) VALUES
+    ('${data}', '${horario}', ${latitude}, ${longitude}, ${sinistroId});`;
 
   console.log("-------------------------");
   console.log(query);
--- routes/getCalls.js
+++ routes/getCalls.js
@@ -30,7 +30,7 @@ date
   /* select id, google_id, data, horario, lat, lon, midia, id_sinistro
 from chamada where data>='11-27-2015' and data<='11-27-2015' and
 horario <= '15:30:00' and horario > '14:00:00';*/
-  var query = `SELECT id, google_id, data, horario, lat, lon, midia, id_sinistro
+  var query = `SELECT id, data, horario, lat, lon, midia, id_sinistro
 FROM chamada WHERE data>='${startDate}'
     AND data<='${endDate}' AND horario <= '${endTime}'
     AND horario >= '${startTime}';`;
